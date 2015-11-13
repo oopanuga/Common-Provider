4165b71e3523caea5d218f7b832e8e18f9aba23d
oopanuga <oopanuga@gmail.com>
2015-11-13 07:14:30 +0000

    Added mmissing SmsProviderBase class

---
+++ Examples/CommonProvider.Example/Providers/SmsProviderBase.cs
@@ -0,0 +1,16 @@
+﻿
+using CommonProvider.Data;
+
+namespace CommonProvider.Example.Providers
+{
+    public abstract class SmsProviderBase : IProvider
+    {
+        public string Name { get; set; }
+
+        public ISettings Settings { get; set; }
+
+        public string Group { get; set; }
+
+        public abstract string SendSms(Message message);
+    }
+}
